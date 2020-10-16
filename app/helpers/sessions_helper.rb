621dd05654d9bc84a8942acd525562dbc2321eb5
Ubuntu <ubuntu@ip-172-31-14-254.eu-west-2.compute.internal>
2020-10-16 15:56:09 +0000

    friday 16 oct

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,26 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    # This creates a permanent, encrypted cookie on the browser with the user id
+    cookies.permanent.signed[:user_id] = user.id
+    # This creates a permanent cookie of the remember token
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
   #returns the current logged in user, if any
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrtyped[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
   
   # returns true if the user is logged in, false if its not
