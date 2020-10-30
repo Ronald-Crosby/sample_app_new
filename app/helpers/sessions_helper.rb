9521c62d61ad9b61390d6a660babe90d87d710ff
Ubuntu <ubuntu@ip-172-31-14-254.eu-west-2.compute.internal>
2020-10-30 16:19:16 +0000

    advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,7 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.encrtyped[:user_id])
+    elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -31,8 +31,16 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  # forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   # Logs out the current user.
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil
   end
